42d41db3d05ffe161ced964d652a361a8a079f87
Ixollozi <mirzaezmichka@gmail.com>
2025-05-06 02:02:44 +0500

    home_page

--- testapp/forms.py
+++ testapp/forms.py
@@ -6,11 +6,35 @@ class TransactionFilterForm(forms.Form):
         ('type', 'По типу'),
         ('category', 'По категории'),
         ('date', 'По дате'),
+
     ]
 
     sort_by = forms.ChoiceField(
         choices=SORT_CHOICES,
         widget=forms.RadioSelect,
         label='Сортировать по',
+        required=False,
+    )
+    DATE_CHOICES = [
+        ('today', 'Сегодня'),
+        ('month', 'Текущий месяц'),
+        ('custom', 'Период'),
+    ]
+
+    period = forms.ChoiceField(
+        choices=DATE_CHOICES,
+        widget=forms.RadioSelect,
+        label='Период',
+        required=False,
+    )
+
+    start_date = forms.DateField(
+        label='С',
+        widget=forms.DateInput(attrs={'type': 'date'}),
+        required=False,
+    )
+    end_date = forms.DateField(
+        label='По',
+        widget=forms.DateInput(attrs={'type': 'date'}),
         required=False,
     )
--- testapp/views.py
+++ testapp/views.py
@@ -1,20 +1,67 @@
+import datetime
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Transaction, Category
 from .forms import TransactionFilterForm
 
-# Create your views here.
 def home_page(request):
     form = TransactionFilterForm(request.GET or None)
     transactions = Transaction.objects.all()
 
     if form.is_valid():
         sort_by = form.cleaned_data.get('sort_by')
+        period = form.cleaned_data.get('period')
+
         if sort_by == 'type':
             transactions = transactions.order_by('type__name')
         elif sort_by == 'category':
             transactions = transactions.order_by('category__category')
-        elif sort_by == 'date':
-            transactions = transactions.order_by('-created_at')
+        elif period == 'today':
+            try:
+                today = datetime.date.today()
+                transactions = transactions.filter(created_at__date=today)
+                if not transactions.exists():
+                    context = {
+                        'form': form,
+                        'message': 'Транзакции за сегодня не найдены'
+                    }
+                    return render(request, 'home.html', context)
+            except Exception as e:
+                context = {
+                    'form': form,
+                    'error': str(e)
+                }
+                return render(request, 'home.html', context)
+        elif period == 'month':
+            try:
+                current_month = datetime.date.today().month
+                transactions = transactions.filter(created_at__month=current_month)
+                transactions = transactions.order_by('created_at')
+                if not transactions.exists():
+                    context = {
+                        'form': form,
+                        'message': 'Транзакции за текущий месяц не найдены'
+                    }
+                    return render(request, 'home.html', context)
+            except Exception as e:
+                context = {
+                    'form': form,
+                    'error': str(e)
+                }
+                return render(request, 'home.html', context)
+        elif period == 'custom':
+            start_date = form.cleaned_data.get('start_date')
+            end_date = form.cleaned_data.get('end_date')
+            transactions = transactions.filter(created_at__range=[start_date, end_date])
+            # фильтрация по порядку
+            transactions = transactions.order_by('created_at')
+            print(transactions)
+            if not transactions.exists():
+                context = {
+                    'form': form,
+                    'message': 'Транзакции за выбранный период не найдены'
+                }
+                return render(request, 'home.html', context)
 
     context = {
         'transactions': transactions,
